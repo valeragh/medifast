5d88d13b1b15d16ae75687c95cb53efe8188f6b0
valeragh <valeragh@gmail.com>
2015-07-26 23:01:33 +0300

    add news controller page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'news/show'
+
   root 'stati_pages#index'
   match '/about_us', to: 'stati_pages#about_us', via: 'get'
   match '/contacts', to: 'stati_pages#contacts', via: 'get'
   match '/faqs', to: 'stati_pages#faqs', via: 'get'
   match '/vacancies', to: 'stati_pages#vacancies', via: 'get'
+  match '/news', to: 'news#index', via: 'get'
 end
