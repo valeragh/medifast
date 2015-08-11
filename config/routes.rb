d92f32ce2f79a685a9223074a193710182982559
valeragh <valeragh@gmail.com>
2015-08-11 10:47:18 +0300

    add devise routing to personals

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :personals
+  devise_for :personals,
+             path: '',
+             path_names: { sign_in: 'login_personal', sign_out: 'logout_personal'}
   devise_for :users,
              controllers: { :omniauth_callbacks => 'users/omniauth_callbacks' },
              path: '',
