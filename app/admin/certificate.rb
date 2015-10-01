9b7642166a79e392d53135fea85bad91b54d2e20
valeragh <valeragh@gmail.com>
2015-10-01 08:03:44 +0300

    edit css and views

--- app/admin/certificate.rb
+++ app/admin/certificate.rb
@@ -4,12 +4,22 @@ ActiveAdmin.register Certificate do
   actions :all, except: [:show]
 
   filter :doctor, label: 'Доктор', as: :select, collection: proc { Doctor.find(Certificate.pluck(:doctor_id)).map { |m| [m.name, m.id] } }
-  
+
+  form do |f|
+    f.inputs 'Доктор' do
+      f.input :doctor_id, as: :select, collection: Doctor.all.map { |m| [m.name, m.id] }
+    end
+    f.inputs 'Изображение маленькое 328X239', :multipart => true do
+      f.input :image_url
+    end
+    actions
+  end
+
   index do
     column("Доктор"){|certificate| certificate.doctor.name}
     column("Сертификат"){|certificate| image_tag certificate.image_url}
     column "Дата создания", :created_at
     actions
-  end  
+  end
 
 end
