d6609a307337ae57b48c44e231ad870886534921
valeragh <valeragh@gmail.com>
2015-11-08 21:32:20 +0200

    add port to thin

--- app/controllers/personals_controller.rb
+++ app/controllers/personals_controller.rb
@@ -1,5 +1,5 @@
 class PersonalsController < ApplicationController
   def index
-    @personals = User.where("role = 'doctor'")
+    @personals = User.where('(role = ? AND rang = ? AND id != ?)', 'doctor', 'Показать', current_user.id)
   end
 end
