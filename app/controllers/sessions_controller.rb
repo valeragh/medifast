1829400d3f4b2390fafe04b9988847bab14a9910
valeragh <valeragh@gmail.com>
2015-11-16 11:24:20 +0200

    add modal for user session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < Devise::SessionsController
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    redirect_to modal_path(modal: 'login')
+  end
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
+    yield if block_given?
+    redirect_to modal_path(modal: 'logout')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users,
-             controllers: { registrations: 'registrations' },
+             controllers: { registrations: 'registrations', sessions: 'sessions' },
              path: '',
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 
