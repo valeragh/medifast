f04abeb09b5a9252c14d46517902df6a09471979
valeragh <valeragh@gmail.com>
2015-12-06 11:01:24 +0200

    add active class to nav and link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_class(link_path)
+    current_page?(link_path) ? "active" : ""
+  end
+
+  def isactive(route)
+    'active' if request.path.try(:starts_with?, route)
+  end
 end
